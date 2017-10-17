06d6209be476a8b36469c89ed97f3dc91478ea41
Julian Valerio <julianrvalerio89@gmail.com>
2017-10-17 20:47:16 -0300

    Sistema aula com inserção no BD

--- app/src/main/java/com/example/julian/sistemaaulas/Cadastrese.java
+++ app/src/main/java/com/example/julian/sistemaaulas/Cadastrese.java
@@ -2,6 +2,10 @@ package com.example.julian.sistemaaulas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Cadastrese extends AppCompatActivity {
 
@@ -9,5 +13,31 @@ public class Cadastrese extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrese);
+        Button cadastrar = (Button)findViewById(R.id.btnConfirmar);
+        cadastrar.setOnClickListener(onClickCadastrar());
+    }
+
+    private View.OnClickListener onClickCadastrar() {
+        return new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //acessar recursos (activity, intent, broadcast, abrir bd...)
+                SistemaAulaDB cadastra = new SistemaAulaDB(getBaseContext());
+                EditText nome = (EditText)findViewById(R.id.edtNomeCad);
+                EditText email = (EditText)findViewById((R.id.edtEmail));
+                EditText dataNasc = (EditText)findViewById(R.id.edtData);
+                EditText senha = (EditText)findViewById(R.id.edtSenhaCad);
+                String nomeString = nome.getText().toString();
+                String emailString = email.getText().toString();
+                String dataNascString = dataNasc.getText().toString();
+                String senhaString = senha.getText().toString();
+                String resultado;
+
+                resultado = cadastra.insereDado(nomeString, emailString,dataNascString,senhaString);
+
+                Toast.makeText(getApplicationContext(), resultado, Toast.LENGTH_LONG).show();
+            }
+        };
+
     }
 }
--- app/src/main/java/com/example/julian/sistemaaulas/Login.java
+++ app/src/main/java/com/example/julian/sistemaaulas/Login.java
@@ -1,30 +1,40 @@
 package com.example.julian.sistemaaulas;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
-
+    public static final String preferencias="sistemaAulas";
+    EditText edtNome;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        edtNome = (EditText)findViewById(R.id.edtNomeCad);
         Button avancar = (Button)findViewById(R.id.entrar);
         avancar.setOnClickListener(onClickEntrar());
+        //Context context = Login.this;
+        //context mode private arquivo criado so pode ser acessado pela propria aplicação
+        SharedPreferences dados =
+                Login.this.getPreferences(Context.MODE_PRIVATE);
+        edtNome.setText(dados.getString("sistemaAulas",""));
     }
 
     private View.OnClickListener onClickEntrar() {
         return new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                TextView login = (TextView) findViewById(R.id.edtNome);
-                TextView senha = (TextView)findViewById(R.id.edtSenha);
+                TextView login = (TextView)findViewById(R.id.edtNomeCad);
+                TextView senha = (TextView)findViewById(R.id.edtSenhaCad);
                 String stringLogin = login.getText().toString();
                 String stringSenha = senha.getText().toString();
                 if(stringLogin.equals("julian") && stringSenha.equals("123")){
@@ -39,6 +49,21 @@ public class Login extends AppCompatActivity {
 
     public void alert(String s){
         Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        CheckBox chkSalvar =
+                (CheckBox)findViewById(R.id.checkBox);
+        if(chkSalvar.isChecked()){
+            SharedPreferences settings =
+                    Login.this.getPreferences(Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putString("sistemaAulas", edtNome.getText().toString());
+            //Confirma a gravação dos dados
+            editor.commit();
+        }
     }
 
 }
